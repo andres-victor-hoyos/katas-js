f4969c358330f0bca616b14b345574fae9ff3687
andres-victor-hoyos <hoyosandres@gmail.com>
2024-07-04 12:25:59 -0300

    Update kata1.js

--- kata1.js
+++ kata1.js
@@ -9,11 +9,11 @@ describe("??????????????????", () => {
   };
 
   const rollSpare = () => {
-    rollMany(2, game.MAX_PINS/2);
+    rollMany(2, game.MAX_PINS / 2);
   };
 
-  const rollStrike = ()=>{
-    rollMany(1,game.MAX_PINS);
+  const rollStrike = () => {
+    rollMany(1, game.MAX_PINS);
   };
 
   test("gutterGame", () => {
@@ -41,46 +41,48 @@ describe("??????????????????", () => {
     expect(game.score()).toBe(20);
   });
 
-  test("perfect game", ()=>{
-    rollMany(12,game.MAX_PINS);
+  test("perfect game", () => {
+    rollMany(12, game.MAX_PINS);
     expect(game.score()).toBe(300);
   });
 });
 
 function Game() {
-    let _rolls = [];
-  
-    const isSpare = (frameIndex) =>
-      _rolls[frameIndex] + _rolls[frameIndex + 1] === this.MAX_PINS;
-  
-    const isStrike = (frameIndex)=>_rolls[frameIndex] === this.MAX_PINS;
-  
-    const strikeBonus = (frameIndex) => _rolls[frameIndex + 1] + _rolls[frameIndex + 2];
-  
-    const spareBonus = (frameIndex) => _rolls[frameIndex + 2];
-  
-    const twoBallsInFrame = (frameIndex) => _rolls[frameIndex] + _rolls[frameIndex + 1];
-  
-    this.MAX_PINS = 10;
-    this.MAX_FRAMES = 10;
-  
-    this.score = () => {
-      let score = 0;
-      let frameIndex = 0;
-      for (let frame = 0; frame < this.MAX_FRAMES; frame++) {
-        if (isStrike(frameIndex)) {
-          score += this.MAX_PINS + strikeBonus(frameIndex);
-          frameIndex++;
-        } else if (isSpare(frameIndex)) {
-          score += this.MAX_PINS + spareBonus(frameIndex);
-          frameIndex += 2;
-        } else {
-          score += twoBallsInFrame(frameIndex);
-          frameIndex += 2;
-        }
+  let _rolls = [];
+
+  const isSpare = (frameIndex) =>
+    _rolls[frameIndex] + _rolls[frameIndex + 1] === this.MAX_PINS;
+
+  const isStrike = (frameIndex) => _rolls[frameIndex] === this.MAX_PINS;
+
+  const strikeBonus = (frameIndex) =>
+    _rolls[frameIndex + 1] + _rolls[frameIndex + 2];
+
+  const spareBonus = (frameIndex) => _rolls[frameIndex + 2];
+
+  const twoBallsInFrame = (frameIndex) =>
+    _rolls[frameIndex] + _rolls[frameIndex + 1];
+
+  this.MAX_PINS = 10;
+  this.MAX_FRAMES = 10;
+
+  this.score = () => {
+    let score = 0;
+    let frameIndex = 0;
+    for (let frame = 0; frame < this.MAX_FRAMES; frame++) {
+      if (isStrike(frameIndex)) {
+        score += this.MAX_PINS + strikeBonus(frameIndex);
+        frameIndex++;
+      } else if (isSpare(frameIndex)) {
+        score += this.MAX_PINS + spareBonus(frameIndex);
+        frameIndex += 2;
+      } else {
+        score += twoBallsInFrame(frameIndex);
+        frameIndex += 2;
       }
-      return score;
-    };
-  
-    this.roll = (pins) => _rolls.push(pins);
-  }
+    }
+    return score;
+  };
+
+  this.roll = (pins) => _rolls.push(pins);
+}
