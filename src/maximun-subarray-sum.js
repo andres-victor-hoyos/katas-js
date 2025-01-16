1787c71440e6c0ac2363c0a0d00e7c000ab2d9b9
Andrés Hoyos <andres.hoyos@naranjax.com>
2025-01-16 08:00:35 -0300

    feat: maximum subarray sum

---
+++ src/maximun-subarray-sum.js
@@ -0,0 +1,13 @@
+
+export default maxSubarraySum = (arr) => {
+    if (arr.length === 0) return 0;
+    if (arr.length === 1) return arr[0];
+    let curr=-Infinity;
+    let ans=-Infinity;
+    for (let i = 0; i < arr.length; i++) {
+        curr = Math.max(arr[i], curr + arr[i]);
+        ans = Math.max(ans, curr);
+    }
+    return ans;
+}
+
---
+++ test/maximun-subarray-sum.test.js
@@ -0,0 +1,15 @@
+import  "../src/maximun-subarray-sum";
+describe("", () => {
+  test("maximun-subarray-sum", () => {
+    expect(maxSubarraySum([])).toBe(0);
+    expect(maxSubarraySum([1])).toBe(1);
+    expect(maxSubarraySum([-1, -2, 3])).toBe(3);
+    expect(maxSubarraySum([1, -2, -3])).toBe(1);
+    expect(maxSubarraySum([-2, 1, -3, 4, -1, 2, 1, -5, 4])).toBe(6);
+    expect(maxSubarraySum([-2, -1, -3, -4, -1, -2, -1, -5, -4])).toBe(-1);
+    expect(maxSubarraySum([-2, 1, -3, 4, -1, 2, 1, -5, 4, 10])).toBe(15);
+    expect(maxSubarraySum([-9,-1,7,-2,5,-20])).toBe(10);
+  });
+});
+
+
