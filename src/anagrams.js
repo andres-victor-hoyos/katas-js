3394deb6f83d2ffb308849c8afaa1ab742b8851b
Andrés Hoyos <andres.hoyos@naranjax.com>
2025-01-18 20:46:41 -0300

    refactor: refactor on anagrams

--- src/anagrams.js
+++ src/anagrams.js
@@ -1,19 +1,11 @@
 export default findAnagrams = (str1, str2) => {
-    if (str1.length !== str2.length) {
-        return false;
-    }
-
     const charCount = {};
-
     for (let char of str1) {
         charCount[char.toLowerCase()] = (charCount[char.toLowerCase()] || 0) + 1;
     }
-
-    for (let char of str2) {
-        if (!charCount[char.toLowerCase()]) {
-            return false;
-        }
+    let j = 0;
+    while (str2[j] && charCount[str2[j].toLowerCase()] && j<str2.length) {
+        j++;        
     }
-
-    return true;
+    return j === str2.length && str1.length === str2.length;
 }
