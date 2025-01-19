63960fbcb1f82080dd5b94cfe5bea94a0ed57661
Andrés Hoyos <andres.hoyos@naranjax.com>
2025-01-18 21:36:11 -0300

    refactor: refactor on anagrams

--- src/maximun-subarray-sum.js
+++ src/maximun-subarray-sum.js
@@ -1,13 +1,11 @@
-
 export default maxSubarraySum = (arr) => {
-    if (arr.length === 0) return 0;
-    if (arr.length === 1) return arr[0];
-    let curr=-Infinity;
-    let ans=-Infinity;
-    for (let i = 0; i < arr.length; i++) {
-        curr = Math.max(arr[i], curr + arr[i]);
-        ans = Math.max(ans, curr);
-    }
-    return ans;
-}
-
+  if (arr.length === 0) return 0;
+  if (arr.length === 1) return arr[0];
+  let curr = -Infinity;
+  let ans = -Infinity;
+  for (let i = 0; i < arr.length; i++) {
+    curr = Math.max(arr[i], curr + arr[i]);
+    ans = Math.max(ans, curr);
+  }
+  return ans;
+};
